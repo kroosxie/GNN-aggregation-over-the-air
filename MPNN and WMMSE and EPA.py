5bcec4d08ac4fa060c1c22277dc93c9486a26e04
kroosxie <kroos_xie@126.com>
2024-01-05 22:17:49 +0800

    对代码进行了一部分注释

--- MPNN and WMMSE and EPA.py
+++ MPNN and WMMSE and EPA.py
@@ -48,7 +48,7 @@ def normalize_data(train_data,test_data):  # 将数据标准化
     tmp_off = (off_diag - off_diag_mean)/off_diag_var
     tmp_off_diag = tmp_off - np.multiply(tmp_off,mask)
     
-    norm_train = np.multiply(tmp_diag,mask) + tmp_off_diag  # 规范化后的数据
+    norm_train = np.multiply(tmp_diag,mask) + tmp_off_diag  # 规范化后的训练集
     
     # normlize test
     mask = np.eye(test_K)
@@ -60,45 +60,45 @@ def normalize_data(train_data,test_data):  # 将数据标准化
     tmp_off = (off_diag - off_diag_mean)/off_diag_var
     tmp_off_diag = tmp_off - np.multiply(tmp_off,mask)
     
-    norm_test = np.multiply(tmp_diag,mask) + tmp_off_diag
+    norm_test = np.multiply(tmp_diag,mask) + tmp_off_diag  # 规范后的测试集
     
     return norm_train, norm_test
 
-def build_graph(loss, norm_loss, K):
-    x1 = np.expand_dims(np.diag(norm_loss),axis=1)
+def build_graph(loss, norm_loss, K):  # 构建图
+    x1 = np.expand_dims(np.diag(norm_loss),axis=1)  # 在列维度上扩充一个维度，方便用于后续转化为张量。np.expand_dims用于在数组的指定轴上扩展维度。
     x2 = np.zeros((K,graph_embedding_size))
-    x = np.concatenate((x1,x2),axis=1)
-    x = torch.tensor(x, dtype=torch.float)
+    x = np.concatenate((x1,x2),axis=1)  # 在列维度上合并
+    x = torch.tensor(x, dtype=torch.float)  # 将数组转化为张量，方便深度学习自动求导等操作
     
-    #conisder fully connected graph
-    loss2 = np.copy(loss)
+    #conisder fully connected graph 构建全连接图
+    loss2 = np.copy(loss)  # 路径损耗副本
     mask = np.eye(K)
-    diag_loss2 = np.multiply(mask,loss2)
-    loss2 = loss2 - diag_loss2
-    attr_ind = np.nonzero(loss2)
-    edge_attr = norm_loss[attr_ind]
-    edge_attr = np.expand_dims(edge_attr, axis = -1)
-    edge_attr = torch.tensor(edge_attr, dtype=torch.float)
+    diag_loss2 = np.multiply(mask,loss2)  # 提取D2D直连链路路径损耗
+    loss2 = loss2 - diag_loss2  # 提取干扰链路
+    attr_ind = np.nonzero(loss2)  # 用于获取数组中非零元素的索引，输出是一个包含两个数组的元组，分别对应行和列的非零元素索引。
+    edge_attr = norm_loss[attr_ind]  # 提取非零干扰链路的标准化值
+    edge_attr = np.expand_dims(edge_attr, axis = -1)  # 在最后一个维度上（列维度）扩充一个维度
+    edge_attr = torch.tensor(edge_attr, dtype=torch.float)  # 边特征，将数组转化为张量，方便深度学习自动求导等操作。
     
-    attr_ind = np.array(attr_ind)
-    adj = np.zeros(attr_ind.shape)
+    attr_ind = np.array(attr_ind)  # 转化为numpy数组，
+    adj = np.zeros(attr_ind.shape)  # 构建邻接矩阵
     adj[0,:] = attr_ind[1,:]
     adj[1,:] = attr_ind[0,:]
-    edge_index = torch.tensor(adj, dtype=torch.long)
+    edge_index = torch.tensor(adj, dtype=torch.long)  # 邻接矩阵
 
     y = torch.tensor(np.expand_dims(loss,axis=0), dtype=torch.float)
-    data = Data(x=x, edge_index=edge_index.contiguous(),edge_attr = edge_attr, y = y)
+    data = Data(x=x, edge_index=edge_index.contiguous(), edge_attr = edge_attr, y=y)  # 使用PYG构件带有节点及边特征值的图。其中.contiguous()可以用来确保张量的内存布局是连续的
     return data
 
 def proc_data(HH, norm_HH, K):
-    n = HH.shape[0]
+    n = HH.shape[0]  # 即layouts的数目
     data_list = []
     for i in range(n):
         data = build_graph(HH[i,:,:], norm_HH[i,:,:], K)
         data_list.append(data)
     return data_list
 
-class GConv(MessagePassing):
+class GConv(MessagePassing):  # 图卷积模块 见文章（6-8）式
     def __init__(self, mlp1, mlp2, **kwargs):
         super(GConv, self).__init__(aggr='max', **kwargs)
         self.mlp1 = mlp1
@@ -176,7 +176,7 @@ def train():
     return total_loss / train_layouts / frame_num
 
 def test():
-    model.eval()
+    model.eval()  # 将模型设置为评估模式：即固定参数
     total_loss = 0
     for data in test_loader:
         data = data.to(device)
@@ -195,8 +195,8 @@ frame_num = 10  # 10帧
 test_config = init_parameters()
 train_config = init_parameters()
 var = train_config.output_noise_power / train_config.tx_power
-frame_length = 3000
-graph_embedding_size = 8
+frame_length = 3000  # 一个frame里面有3000个symbol
+graph_embedding_size = 8  #
 overhead_csi = 1
 overhead_mp = 5
 
@@ -204,7 +204,7 @@ print('Data generation')
 #Data generation
 #Train data
 layouts, train_dists = wg.generate_layouts(train_config, train_layouts)  # 创建训练集个数的tx、rx分布以及所有链路的距离信息（相当于生成训练集个数的地图）
-train_path_losses = wg.compute_path_losses(train_config, train_dists)  # 计算所有链路的路径损耗的绝对值
+train_path_losses = wg.compute_path_losses(train_config, train_dists)  # 计算所有链路的路径损耗的绝对值，这里的loss是path_loss，不是loss_function
 train_channel_losses = helper_functions.add_fast_fading_sequence(frame_num, train_path_losses)  # 在每一个帧加入快衰落
 #Treat multiple frames as multiple samples for MPNN 在MPNN中将多个帧视为多个采样
 train_channel_losses = train_channel_losses.reshape(train_layouts*frame_num,train_K,train_K)  # 信道系数作为训练集
@@ -215,17 +215,17 @@ test_channel_losses = helper_functions.add_fast_fading_sequence(frame_num,test_p
 #Treat multiple frames as multiple samples for MPNN
 test_channel_losses = test_channel_losses.reshape(test_layouts*frame_num,test_K,test_K)
 
-#Data normalization
+#Data normalization 数据规范化
 norm_train_losses, norm_test_losses = normalize_data(np.sqrt(train_channel_losses),np.sqrt(test_channel_losses) )
 print('Graph data processing')
-#Graph data processing
-train_data_list = proc_data(train_channel_losses, norm_train_losses, train_K)
+#Graph data processing 图数据处理
+train_data_list = proc_data(train_channel_losses, norm_train_losses, train_K)  # 使用PYG生成带节点及边特征的图
 test_data_list = proc_data(test_channel_losses, norm_test_losses, test_K)
 
 
 directLink_channel_losses = helper_functions.get_directLink_channel_losses(test_channel_losses)
 crossLink_channel_losses = helper_functions.get_crossLink_channel_losses(test_channel_losses)
-mpnn_overhead_ratio = (frame_length-overhead_csi*train_K*train_K-overhead_mp*(train_K)*3)/frame_length
+mpnn_overhead_ratio = (frame_length-overhead_csi*train_K*train_K-overhead_mp*(train_K)*3)/frame_length  # *3？
 wmmse_overhead_ratio = (frame_length-overhead_csi*train_K*train_K)/frame_length
 print('WMMSE and EPA computation')
 #sum rate of wmmse
@@ -233,10 +233,10 @@ Pini = np.random.rand(test_layouts*frame_num,test_K,1 )
 Y2 = helper_functions.batch_WMMSE(Pini,np.ones([test_layouts*frame_num, test_K]),np.sqrt(test_channel_losses),1,var)
 rates_wmmse = helper_functions.compute_rates(test_config, 
             Y2, directLink_channel_losses, crossLink_channel_losses)
-sum_rate_wmmse = np.mean(np.sum(rates_wmmse,axis=1))*wmmse_overhead_ratio
+sum_rate_wmmse = np.mean(np.sum(rates_wmmse,axis=1))*wmmse_overhead_ratio   # *wmmse_overhead_ratio以计算有效通信的速率
 print('WMMSE average sum rate:',sum_rate_wmmse)
 
-#sum rate of epa
+#sum rate of epa 全1功率发射
 Pepa = np.ones((test_layouts*frame_num,test_K))
 rates_epa = helper_functions.compute_rates(test_config, 
             Pepa, directLink_channel_losses, crossLink_channel_losses)
